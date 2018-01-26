a36c1d285ea2cb6c1234604710edb2f9baf3401c
Antonio <antonio.ruggiero93@hotmail.it>
2018-01-27 00:11:47 +0100

    test blendshape, gameManager e Eventmanager

---
+++ Assets/EventManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static EventManager Instance;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+
+    public UnityEvent OnInfectWaveFinished;
+
+    void Start()
+    {
+        OnInfectWaveFinished=new UnityEvent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+	public float InfectDuration;
+	public Image InfectTimeFilledImage;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			StartInfectWave();
+		}
+	}
+
+
+	void StartInfectWave()
+	{
+		InfectTimeFilledImage.fillAmount = 1;
+		InfectTimeFilledImage.DOFillAmount(0, InfectDuration).OnComplete(() =>
+		{
+			EventManager.Instance.OnInfectWaveFinished.Invoke();
+			Debug.Log("Start next wave!");
+		});
+	}
+	
+}
