aedae8e8b72ed1a718ba10beb1e6f57073779675
probobbo <probobbo@gmail.com>
2018-01-27 11:03:19 +0100

    creato playerpowerup

---
+++ Assets/Scripts/PlayerPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPowerUp : MonoBehaviour {
+
+	PlayerStatus Status;
+	CharacterMovement Movement;
+
+	// Use this for initialization
+	void Start () {
+		Status = GetComponent<PlayerStatus>();
+		Movement = GetComponent<CharacterMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
