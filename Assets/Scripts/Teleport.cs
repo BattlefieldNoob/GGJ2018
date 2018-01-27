88a6d8504f3b391e400465652868f8be21f7af28
mdallapi <massimo.dallapieta@hotmail.it>
2018-01-27 01:09:23 +0100

    aggiunto script teleport

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void OnCollisionEnter(Collider other)
+    public enum Type
     {
+        upper = 0,
+        lower = 1,
+        right = 2,
+        left = 3
+    }
 
+    public Type Border;
+
+    public float width;
+
+    public float height;
+
+    private List<Vector3> axis = new List<Vector3>();
+
+
+    public void Start()
+    {
+        axis =  new List<Vector3>
+        {
+            new Vector3( 0, 0, height),
+            new Vector3( 0, 0, -height),
+            new Vector3( width, 0, 0),
+            new Vector3( -width, 0, 0),
+        };
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 pos = collision.gameObject.transform.position;
+        switch (Border)
+        {
+            case Type.upper: collision.gameObject.transform.position += axis[0];
+                break;
+            case Type.lower: collision.gameObject.transform.position += axis[1];
+                break;
+            case Type.right: collision.gameObject.transform.position += axis[2];
+                break;
+            case Type.left: collision.gameObject.transform.position += axis[3];
+                break;
+        }
     }
 }
