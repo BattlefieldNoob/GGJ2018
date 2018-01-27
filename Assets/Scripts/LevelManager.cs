aa3a301683f24ad68722c7f97ee3c99ae3cd6782
Matteo Manicone <teomani17@gmail.com>
2018-01-27 09:16:10 +0100

    Menu
    Aggiunta scena menu, il fader e script per il cambio di scena.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour {
+
+	public  void LoadOnSceneIndex(int sceneIndex){
+
+		SceneManager.LoadScene (sceneIndex);
+		Debug.Log (" Loading  scene number " + sceneIndex);		
+	}
+
+	public void LoadOnSceneName(string sceneName){
+		
+		SceneManager.LoadScene (sceneName);
+		Debug.Log (" Loading  scene " + sceneName);
+	}
+
+	public void CloseGame (){
+		Application.Quit ();
+		Debug.Log ("Closing the Application");
+	}
+}
