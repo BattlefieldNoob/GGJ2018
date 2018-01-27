9767141a66c3bd98e2906fca2d4d43362ad350e1
probobbo <probobbo@gmail.com>
2018-01-27 09:42:01 +0100

    aggiunti assi giocatori con controller diversi

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class CharacterMovement : MonoBehaviour {
 
 	public float Speed;
+	public int PlayerID;
 	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
@@ -14,7 +15,11 @@ public class CharacterMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 dir = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical")).normalized;
+		Vector3 dir = new Vector3(Input.GetAxis("HorizontalPL"+PlayerID),0,Input.GetAxis("VerticalPL"+PlayerID)).normalized;
+		foreach(string s in Input.GetJoystickNames())
+		{
+			print(s);
+		}
 		if (dir != Vector3.zero)
 		{
 			transform.rotation = Quaternion.LookRotation(dir);
