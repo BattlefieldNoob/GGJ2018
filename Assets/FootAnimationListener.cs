da779989a4a01a7683b4669c0d7e7cb6d0315473
Antonio <antonio.ruggiero93@hotmail.it>
2018-01-28 11:04:48 +0100

    Rimosso dash da Animator e aggiunti eventi Foot

---
+++ Assets/FootAnimationListener.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootAnimationListener : MonoBehaviour {
+
+	public void OnFoot()
+	{
+		//Debug.Log("Foot!");
+	}
+}
