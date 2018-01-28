9da11ce0e5209ceeba336608170386d3d8460ea8
mdallapi <massimo.dallapieta@hotmail.it>
2018-01-28 05:07:17 +0100

    inserito codice dash

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,6 +11,8 @@ public class CharacterMovement : MonoBehaviour
 
 	public bool CanMove = true;
 
+    public bool CanDash = true;
+
 	private Animator Animator;
 	public ParticleSystem WalkParticle;
 
@@ -25,7 +27,23 @@ public class CharacterMovement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		if (CanMove)
+        // TEST DASH BRUH
+        if (CanDash && CanMove)
+        {
+            CanMove = false;
+            Vector3 dash = new Vector3(Input.GetAxis("HorizontalPR" + PlayerID), 0, Input.GetAxis("VerticalPR" + PlayerID)).normalized;
+            if (dash != Vector3.zero)
+            {
+                CanDash = false;
+                StartCoroutine(dashMove(dash));
+                StartCoroutine(dashCooldown());
+            }
+            else
+            {
+                CanMove = true;
+            }
+        }
+        if (CanMove)
 		{
 			Vector3 dir = new Vector3(Input.GetAxis("HorizontalPL" + PlayerID), 0, Input.GetAxis("VerticalPL" + PlayerID)).normalized;
 			if (dir != Vector3.zero)
@@ -41,8 +59,24 @@ public class CharacterMovement : MonoBehaviour
 				if(WalkParticle.isPlaying)
 					WalkParticle.Stop(true,ParticleSystemStopBehavior.StopEmitting);
 			}
-		}
-		
+        }
 		Animator.SetFloat("Velocity",rb.velocity.magnitude/Status.GetSpeed());
 	}
+
+    private IEnumerator dashMove(Vector3 dash)
+    {
+        rb.velocity = Vector3.zero;
+        transform.rotation = Quaternion.LookRotation(dash);
+        rb.AddForce(dash * 10, ForceMode.Impulse);
+        Animator.SetTrigger("Dash");
+        yield return new WaitForSeconds(0.5f);
+        rb.velocity = Vector3.zero;
+        CanMove = true;
+    }
+
+    private IEnumerator dashCooldown()
+    {
+        yield return new WaitForSeconds(2.0f);
+        CanDash = true;
+    }
 }
