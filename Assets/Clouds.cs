44e3a60286312a97520e356492779531f452d23a
Antonio <antonio.ruggiero93@hotmail.it>
2018-01-28 04:35:57 +0100

    Script Night club

---
+++ Assets/Clouds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+
+	void Start () {
+		foreach (Transform child in transform)
+		{
+			child.DOMoveX(-30, 40f).SetLoops(-1, LoopType.Yoyo).SetDelay(Random.Range(1, 4));
+		}
+	}
+	
+}
---
+++ Assets/Donnina.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Donnina : MonoBehaviour
+{
+    private Material donninaMat;
+    private Material insegnaMat;
+    public Texture donnina1;
+    public Texture donnina2;
+
+    void Start()
+    {
+        donninaMat = GetComponent<MeshRenderer>().materials[3];
+        insegnaMat = GetComponent<MeshRenderer>().materials[0];
+        StartCoroutine(Blink());
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            donninaMat.mainTexture = donnina1;
+            yield return new WaitForSeconds(0.7f);
+            donninaMat.mainTexture = donnina2;
+            yield return new WaitForSeconds(0.7f);
+
+            var random = Random.Range(0f, 1f);
+
+            if (random > 0.75f)
+            {
+                insegnaMat.DOFade(0f, 0.1f).SetEase(Ease.InOutSine).OnComplete(() =>
+                    {
+                        insegnaMat.DOFade(1f, 0.1f).SetEase(Ease.InOutSine);
+                    });
+            }
+        }
+    }
+}
